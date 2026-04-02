61303a32c2644e07039ea100a42c9ae4ec28822b
hayasaka0v0 <huyham1511@gmail.com>
2026-04-03 00:15:56 +0700

    test

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-def main():
-    print("Hello from datn-signlanguagedetection!")
+# import os # Thư viện tương tác với hệ điều hành
 
 
-if __name__ == "__main__":
-    main()
+
+# list_video = os.listdir("Projects/DATN/DATN_SignLanguageDetection/data/raw_videos/")
+# for folder in list_video:
+#     print("Title: " + folder)
+#     print(os.listdir("Projects/DATN/DATN_SignLanguageDetection/data/raw_videos/" + folder))
+#     print("\n")
+
+
+import os # Thư viện tương tác với hệ điều hành
+
+base_path = "Projects/DATN/DATN_SignLanguageDetection/data/raw_videos/"
+
+list_video = os.listdir(base_path)
+for folder in list_video:
+    print("Title: " + folder)
+    full_path = os.path.join(base_path, folder)
+    for video in os.listdir(full_path):
+        if video.endswith(".mp4") or video.endswith(".webm"):
+            print(video)
+    print("\n")
